185b7cb9079af4662c212d66887e4271a90126f0
Ярослава Коваленко < >
2016-11-23 17:14:43 +0200

    Start version

---
+++ js/timer.js
@@ -0,0 +1,7 @@
+(function(){
+	var _id="87d5123d4928c95cdf11048670a53bfa";
+	while(document.getElementById("timer"+_id))_id=_id+"0";
+	document.write("<div id='timer"+_id+"' style='min-width:282px;height:73px;'></div>");
+	var _t=document.createElement("script");_t.src="http://megatimer.ru/timer/timer.min.js";
+	var _f=function(_k)
+	{var l=new MegaTimer(_id, {"view":[0,1,1,1],"type":{"currentType":"2","params":{"startByFirst":true,"days":"0","hours":"0","minutes":"1","utc":0}},"design":{"type":"text","params":{"number-font-family":{"family":"Comfortaa","link":"<link href='http://fonts.googleapis.com/css?family=Comfortaa&subset=latin,cyrillic' rel='stylesheet' type='text/css'>"},"number-font-size":"60","number-font-color":"#000","separator-margin":"20","separator-on":true,"separator-text":":","text-on":true,"text-font-family":{"family":"Comfortaa","link":"<link href='http://fonts.googleapis.com/css?family=Comfortaa&subset=latin,cyrillic' rel='stylesheet' type='text/css'>"},"text-font-size":"12","text-font-color":"#c7c7c7"}},"designId":1,"theme":"white","width":282,"height":73});if(_k!=null)l.run();};_t.onload=_f;_t.onreadystatechange=function(){if(_t.readyState=="loaded")_f(1);};var _h=document.head||document.getElementsByTagName("head")[0];_h.appendChild(_t);}).call(this);
